fbc7c59f30db060f9f0fd60ef6a65ca5c3acc3c5
Rishik Jariwala <rishikjariwala54@gmail.com>
2026-05-24 17:50:24 +0530

    DEPENDECNY INJECTION Commit

--- myFirstProjectSpringboot/src/main/java/com/rishik/myFirstProjectSpringboot/MyCar.java
+++ myFirstProjectSpringboot/src/main/java/com/rishik/myFirstProjectSpringboot/MyCar.java
@@ -1,13 +1,18 @@
 package com.rishik.myFirstProjectSpringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
 @RestController
 public class MyCar {
+//    public String car(){
+//    return "MycAR";}
+    @Autowired
+    private myDog dog;
     @GetMapping("car")
     public String car(){
-    return "MycAR";}
+        return dog.bark();
+    }
 }
---
+++ myFirstProjectSpringboot/src/main/java/com/rishik/myFirstProjectSpringboot/myDog.java
@@ -0,0 +1,11 @@
+package com.rishik.myFirstProjectSpringboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class myDog
+{
+    public String bark(){
+        return "HelloDog";
+    }
+}
